dec707e27dabd8e19ec2c92a1f3d9f34b57cebe2
Chandan Kumar Mandal <chandankumarm204@gmail.com>
2025-12-01 18:32:36 +0530

    AWT (token)

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -25,5 +25,5 @@ const UserSchema = new Schema({
 
 
 });
-
-module.exports = mongoose.model('user', UserSchema);
+const User = mongoose.model('user', UserSchema);
+module.exports = User;
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,15 +2,44 @@ const express = require('express');
 const User = require('../models/User');
 
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
+//Create a user using: POST "/api/auth/createuser" . No loging required
+router.post('/createuser', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password').isLength({ min: 5 }),
+], async (req, res) => {
+    //if there are error, return Bad error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-//Create a user using: POST "/api/auth/" . Doesn't require auth
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body);
-}
+    //check whether email exists already
+    try {
 
-)
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry an email already exists" })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+
+        const secPass = await bcrypt.hash(req.body.password, salt)
+        //Create a new user
+        user = await User.create({
+            name: req.body.name,
+            password: secPass,
+            email: req.body.email,
+        })
+
+        res.json({ user })
+    } catch (error) {
+        res.status(500).send("Some error occ");
+
+    }
+
+})
 module.exports = router
